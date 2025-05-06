44aa28746e75351bcba985376ac83d0860d0d179
Rohethan <40922773+Rohethan@users.noreply.github.com>
2025-05-06 13:51:54 +0200

    Bugfix in homebrew counter implementation

--- libraries/counter.py
+++ libraries/counter.py
@@ -1,14 +1,10 @@
 import machine
 
-
 class Counter:
-    def increment(self):
+    def increment(self, pin):  # Accept the IRQ argument.
         self.count += 1
 
     def __init__(self, pin, start=0):
         self.count = start
         self.pin = machine.Pin(pin, machine.Pin.IN)
         self.pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=self.increment)
-
-
-
